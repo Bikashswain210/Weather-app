f7a148fa8e24b346469c89f159287cdea9178371
Bikash Kumar Swain <bikashswain210@gmail.com>
2022-10-16 20:38:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const searchInputBox =document.getElementById('Input-Box');
+searchInputBox.addEventListener('keypress',(event)=>{
+    if(even.keycode == 13){
+    console.log(searchInputBox.value);
+}
+});
+
+//getweathre report
+function getweatherReport(city){
+    fetch('')
+
